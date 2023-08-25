d78cbe4a6ab265fcc2a44fd7088b8f0079303485
eduardo manczenko <eduardo.manczenko@gmail.com>
2023-08-24 21:18:06 -0300

    fix: Card3

---
+++ src/Components/Card3/index.tsx
@@ -0,0 +1,66 @@
+import Polygon from "../Icons/Polygon";
+import Wallet from "../Icons/Wallet";
+
+export default function Card3() {
+  return (
+    <div className="flex flex-col  bg-white w-[350px] h-[300px] rounded-lg  shadow-2xl ">
+      <div className="flex flex-col  text-black h-full shadow-2xl">
+
+        <div className="w-full h-[50%] relative">
+          <img src="/images/Vicon.svg" className="absolute right-2 p-1 pt-2" alt="" />
+          <img src="/images/vaquinha2.png" className="h-full w-full rounded-t-lg" alt="" />
+        </div>
+
+        <div className="flex items-center justify-between py-1 mr-[5%] ml-[5%]">
+          <h1 className="text-black font-Archivo font-bold text-18 leading-normal tracking-wide">Nelore#2540</h1>
+          <div>
+            <Polygon/>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between mr-[5%] ml-[5%]">
+
+          <div className="flex items-center justify-between shadow-xl my-2 py-1 w-[40%]">
+            <h1 className="text-black font-Archivo text-14 font-normal leading-normal tracking-wider">USDT</h1>
+            <h2 className="text-black font-Archivo text-16 font-normal leading-normal tracking-wider">$20</h2>
+          </div>
+
+          <div className="flex items-center justify-between shadow-xl my-2 py-1 w-[40%]">
+            <h1 className="text-black font-Archivo text-14 font-normal leading-normal tracking-wider">BRL</h1>
+            <h2 className="text-black font-Archivo text-16 font-normal leading-normal tracking-wider">R$100</h2>
+          </div>
+
+        </div>
+
+        <div className="flex gap-2 items-center mr-[7%] ml-[7%] mb-[5px]">
+          <Wallet/>
+          <h1 className="text-black font-Archivo text-base font-normal leading-normal tracking-wider">cro41.....9012</h1>
+        </div>
+
+        <div className="flex flex-col pb-[6px] rounded-b-lg bg-gradient-to-r from-[#7000FF] to-[#B338FF]">
+
+          <div className="flex items-center gap-2 justify-between mr-[15%] ml-[15%] pt-[4%] pb-[2%]">
+
+            <div className="flex flex-col items-center bg-black rounded-md w-[30%] pt-[2%] pb-[2%] bg-opacity-40 backdrop-blur-md">
+              <h1 className="text-white font-Archivo text-base font-normal leading-normal tracking-wider">3</h1>
+              <h2 className="text-white font-Archivo text-xs font-normal leading-normal tracking-wider">dias</h2>
+            </div>
+            <div className="flex flex-col items-center bg-black rounded-md w-[30%] pt-[2%] pb-[2%] bg-opacity-40 backdrop-blur-md">
+              <h1 className="text-white font-Archivo text-base font-normal leading-normal tracking-wider">5</h1>
+              <h2 className="text-white font-Archivo text-xs font-normal leading-normal tracking-wider">horas</h2>
+            </div>
+            <div className="flex flex-col items-center bg-black rounded-md w-[30%] pt-[2%] pb-[2%] bg-opacity-40 backdrop-blur-md">
+              <h1 className="text-white font-Archivo text-base font-normal leading-normal tracking-wider">20</h1>
+              <h2 className="text-white font-Archivo text-xs font-normal leading-normal tracking-wider">min</h2>
+            </div>
+            <div className="flex flex-col items-center bg-black rounded-md w-[30%] pt-[2%] pb-[2%] bg-opacity-40 backdrop-blur-md">
+              <h1 className="text-white font-Archivo text-base font-normal leading-normal tracking-wider">53</h1>
+              <h2 className="text-white font-Archivo text-xs font-normal leading-normal tracking-wider">seg</h2>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
