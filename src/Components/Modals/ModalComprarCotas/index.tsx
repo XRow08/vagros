32b221821f42b4c1836944f75973800fa104b058
XRow08 <murilloaugustoe081@gmail.com>
2023-10-20 13:11:04 -0300

    min value

--- src/Components/Modals/ModalComprarCotas/index.tsx
+++ src/Components/Modals/ModalComprarCotas/index.tsx
@@ -24,7 +24,7 @@ export default function ModalComprarCotas() {
   );
 
   useEffect(() => {
-    setPriceCicle(selectedCicle?.curValue.toString()!);
+    setPriceCicle(selectedCicle?.minValue.toString()!);
   }, []);
 
   return (
